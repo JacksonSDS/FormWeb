76495294f66c0a0fce86c981a2e7f6890529ac40
jacksonsantos <jacksonsantos@magnasistemas.com.br>
2024-02-06 13:43:49 -0300

    verificação do email unico

--- js/dados.js
+++ js/dados.js
@@ -2,7 +2,6 @@ export async function Dados() {
   let nome = document.getElementById('nome').value;
   let email = document.getElementById('email').value;
 
-// Verificar se o email já existe
 let usuarioExistente = await fetch(`http://localhost:3000/usuarios?email=${email}`)
   .then(response => response.json())
   .catch(error => console.error('Erro:', error));
@@ -37,7 +36,7 @@ if (usuarioExistente.length > 0) {
   .then(response => response.json())
   .then(data => {
     console.log(data);
-    limparFormulario(); // Limpar formulário após envio bem-sucedido, se desejado.
+    limparFormulario(); 
   })
   .catch(error => {;
   });
